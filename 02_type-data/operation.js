0dbc5ef235c1b298ec244afe1432bf4219f36579
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-03-15 20:08:14 +0700

    add operator

---
+++ 02_type-data/operation.js
@@ -0,0 +1,8 @@
+// operator itu selalu boolean
+// |>, |<
+// console.log(1 < 2) // true
+// console.log(2 > 3) // false
+// console.log(10 <= 10) // karna ada sama dengan 
+
+// console.log("" === "")
+// console.log("2" !== 2) // "2" itu tidak mirip 2
